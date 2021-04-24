1db3000bc38889eef483ed934219b50b8a775558
ThePVY <polagr@yandex.ru>
2021-04-25 00:48:47 +0300

    added physic effects and controls

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   const container = document.querySelector('#app-wrapper header')
-  const needle = new Needle()
+  needle.pointX = window.innerWidth / 2
   needle.pointY = window.innerHeight - container.offsetHeight
   needle.node = document.querySelector('#app-wrapper header img')
   mouseWatcher.startWatch(container, (Xm = 0, Ym = 0, direction = 'left') => {
@@ -9,33 +9,34 @@ window.onload = () => {
     needle.pointX = direction === 'right' ? Xm + 52 : Xm
   })
 
-  const bubbleArea = document.querySelector('#app-wrapper')
-  view.container = bubbleArea
+  view.bubbleAreaNode = document.querySelector('#app-wrapper')
+  view.bubblesScoreNode = document.querySelector('#app-wrapper .bubbles')
+  view.missesScoreNode = document.querySelector('#app-wrapper .misses')
+  view.puncturesScoreNode = document.querySelector('#app-wrapper .punctures')
 
   model.needle = needle
 
-  controller.start()
+  const startButton = document.querySelector('.restart-button-container button')
+  controller.startButtonNode = startButton
+  startButton.addEventListener('click', () => {
+    controller.start()
+    startButton.disabled = true
+  })
 }
 
-/* (Xm, Ym,) => {
-  const distance = Math.sqrt((bubble.x - Xm) ** 2 + (bubble.y - Ym) ** 2)
-  if (distance < bubble.radius) {
-    console.log("Puncture")
-  }
-} */
-
-function Needle() {
-  this.pointX = null
-  this.pointY = null
-  this.node = null
+/*-----------------------MODEL LAYER----------------------*/
+const needle = {
+  pointX: null,
+  pointY: null,
+  node: null
 }
 
-function Bubble(x, y, width, height) {
+function Bubble(x, y, size) {
   this.x = x //abs coordinates of center
   this.y = y //abs coordinates of center
-  this.width = width
-  this.height = height
-  this.radius = width / 2
+  this.width = size
+  this.height = size
+  this.radius = size / 2
   this.node = document.createElement('div')
   view.renderBubble(this)
 }
@@ -62,52 +63,62 @@ const mouseWatcher = {
 }
 
 const model = {
-  width: 70,
-  height: 70,
   needle: null,
   bubbles: [],
   createBubble() {
-    const x = this.width * 2 + Math.random() * (window.innerWidth - this.width * 4)
-    const y = this.height * 4
-    this.bubbles.push(new Bubble(x, y, this.width, this.height))
+    const size = 50 + Math.random() * 200
+    const x = size + Math.random() * (window.innerWidth - 2 * size)
+    const y = size / 2 + window.innerHeight * 0.1
+    this.bubbles.push(new Bubble(x, y, size))
+    scoreController.bubbleCreated()
   },
-  removeBubbles() {
-    this.bubbles.forEach(function(bubble) { bubble.node.remove() })
+  removeBubbleNodes() {
+    this.bubbles.forEach(function (bubble) { this.removeBubbleNode(bubble) }.bind(this))
+  },
+  removeBubbleNode(bubble) {
+    bubble.node.remove()
   },
   checkPuncture() {
-    this.bubbles.forEach(function(bubble) {
+    for (let i = 0; i < this.bubbles.length; i++) {
+      const bubble = this.bubbles[i]
       const distance = Math.sqrt((bubble.x - this.needle.pointX) ** 2 + (bubble.y - this.needle.pointY) ** 2)
       if (distance < bubble.radius) {
-        console.log("Puncture")
         view.hide(bubble)
+        scoreController.bubblePunctured()
+        this.removeBubbleNode(bubble)
+        this.removeBubble(bubble)
+        return
       }
-    }.bind(this))
+    }
+  },
+  checkMisses() {
+    for (let i = 0; i < this.bubbles.length; i++) {
+      const bubble = this.bubbles[i]
+      if (bubble.y >= window.innerHeight + bubble.width) {
+        scoreController.bubbleMissed()
+        this.removeBubbleNode(bubble)
+        this.removeBubble(bubble)
+        return
+      }
+    }
+  },
+  removeBubble(bubble) {
+    this.bubbles = this.bubbles.filter((b) => b !== bubble)
+  },
+  reset() {
+    this.bubbles = []
   }
 }
-
-const hoistController = {
-  stepInterval: null,
-  bubbles: model.bubbles,
-  directionX: 1,
-  stepX: 0,
-  stepY: 4,
-  startHoisting() {
-    this.stepInterval = setInterval(function () {
-      this.bubbles.forEach((bubble) => {
-        bubble.x += this.stepX
-        bubble.y += this.stepY
-      })
-      view.updateAll()
-      model.checkPuncture()
-    }.bind(this), 30)
-  }
-}
-
+/*-----------------------VIEW LAYER----------------------*/
 const view = {
-  container: null,
+  bubbleAreaNode: null,
+  bubblesScoreNode: null,
+  missesScoreNode: null,
+  puncturesScoreNode: null,
   renderBubble(bubble) {
     this.update(bubble)
-    this.container.appendChild(bubble.node)
+    bubble.node.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+    this.bubbleAreaNode.appendChild(bubble.node)
   },
   updateAll() {
     model.bubbles.forEach((bubble) => this.update(bubble))
@@ -125,27 +136,167 @@ const view = {
   },
   hide(bubble) {
     bubble.node.style.display = 'none'
+  },
+  displayBubblesScore() {
+    this.bubblesScoreNode.innerHTML = `bubbles: ${scoreController.bubbles}`
+  },
+  displayMissesScore() {
+    this.missesScoreNode.innerHTML = `misses: ${scoreController.misses}`
+  },
+  displayPuncturesScore() {
+    this.puncturesScoreNode.innerHTML = `punctures: ${scoreController.punctures}`
+  },
+  displayScore() {
+    this.displayBubblesScore()
+    this.displayMissesScore()
+    this.displayPuncturesScore()
+  }
+}
+/*-----------------------CONTROL LAYER----------------------*/
+const defaultStepY = 2
+
+const hoistController = {
+  hoistingOnGoing: true,
+  stepInterval: null,
+  stepY: defaultStepY,
+  stepCount: 0,
+  startHoisting() {
+    this.hoistingOnGoing = true;
+    this.stepCount = 0
+    windController.start()
+    this.stepInterval = setInterval(function () {
+      model.bubbles.forEach((bubble) => {
+        bubble.x += this.willBeInRange(bubble) ? this.getStep(bubble, windController.step) : 0
+        bubble.y += this.stepY
+      })
+      view.updateAll()
+      model.checkPuncture()
+      model.checkMisses()
+      this.countStep()
+      this.speedUp()
+      if (!model.bubbles.length && !scoreController.counting) {
+        controller.onStopHoisting()
+      }
+    }.bind(this), 20)
+  },
+  willBeInRange(bubble) {
+    const nextX = bubble.x + windController.step
+    return nextX > bubble.radius && nextX < window.innerWidth - bubble.radius ? true : false
+  },
+  getStep(bubble, step) {
+    return windController.direction === 1 ? 
+      step * (window.innerWidth - bubble.radius - bubble.x) / (window.innerWidth - bubble.radius)
+      :
+      step * bubble.x / (window.innerWidth - bubble.radius)
+  },
+  countStep() {
+    this.stepCount++
+  },
+  speedUp() {
+    (this.stepCount % 500 === 0) && this.stepY++
+  },
+  reset() {
+    clearInterval(this.stepInterval)
+    this.hoistingOnGoing = false;
+    this.stepCount = 0;
+    this.stepX = defaultStepX
+    this.stepY = defaultStepY
   }
 }
 
-const controller = {
-  creationInterval: null,
+const defaultDirection = 1
+const defaultStepX = 5
+const defaultWindTime = 5000
+
+const windController = {
+  direction: defaultDirection,
+  step: defaultDirection * defaultStepX,
+  windTime: defaultWindTime,
   start() {
-    this.creationInterval = setInterval(function () {
-      model.createBubble()
-    }.bind(this), 1000)
+    setTimeout(this.changeTheWind.bind(this), this.windTime)
+  },
+  changeTheWind() {
+    this.direction = Math.round(Math.random()) ? 1 : -1
+    //this.direction = defaultDirection
+    this.step = this.direction * Math.round(Math.random() * defaultStepX)
+    //this.step = this.direction * defaultStepX
+    this.windTime = 500 + Math.round(Math.random() * (defaultWindTime - 500))
+    hoistController.hoistingOnGoing && setTimeout(this.changeTheWind.bind(this), this.windTime)
+  },
+  reset() {
+    this.direction = defaultDirection
+    this.step = defaultStepX
+    this.windTime = defaultWindTime
+  }
+}
+
+const scoreController = {
+  bubbles: 0,
+  punctures: 0,
+  misses: 0,
+  counting: true,
+  bubbleCreated() {
+    this.bubbles++
+    this.counting && view.displayBubblesScore()
+    console.log(`bubbles: ${this.bubbles}`)
+  },
+  bubbleMissed() {
+    this.misses++
+    this.counting && view.displayMissesScore()
+    console.log(`misses: ${this.misses}`)
+  },
+  bubblePunctured() {
+    this.punctures++
+    this.counting && view.displayPuncturesScore()
+    console.log(`punctures: ${this.punctures}`)
+  },
+  reset() {
+    this.bubbles = 0
+    this.punctures = 0
+    this.misses = 0
+    this.counting = true
+  }
+}
+
+const defaultCreationTime = 3000
+
+const controller = {
+  startButtonNode: null,
+  creationOnGoing: true,
+  creationTime: defaultCreationTime,
+  start() {
+    this.creationOnGoing = true
+    view.displayScore()
+
+    setTimeout(this.bubbleCreator.bind(this), this.creationTime)
 
     hoistController.startHoisting(model.bubbles)
 
     setTimeout(function () {
-      this.stop()
-    }.bind(this), 20000)
+      this.stopCreation()
+    }.bind(this), 60000)
   },
-  stop() {
-    clearInterval(this.creationInterval)
-    clearInterval(hoistController.stepInterval)
-    model.removeBubbles()
-    model.bubbles = null
-    hoistController.bubbles = null
+  stopCreation() {
+    this.creationTime = defaultCreationTime
+    this.creationOnGoing = false
+    scoreController.counting = false
+  },
+  onStopHoisting() {
+    model.removeBubbleNodes()
+    model.reset()
+    scoreController.reset()
+    windController.reset()
+    hoistController.reset()
+    this.startButtonNode.disabled = false
+  },
+  bubbleCreator() {
+    model.createBubble()
+    if (this.creationTime >= 300) {
+      this.creationTime *= 0.95
+    }
+    this.creationOnGoing && setTimeout(this.bubbleCreator.bind(this), this.creationTime)
+
   }
 }
+
+
